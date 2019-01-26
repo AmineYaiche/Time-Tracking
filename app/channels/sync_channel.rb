37930f9647dfa91665faab3813b6d91a5a65ad2d
Amine Yaiche <yaiche.amin@gmail.com>
2019-01-26 12:48:40 +0100

    Implementing server side of time tracking

--- app/channels/sync_channel.rb
+++ app/channels/sync_channel.rb
@@ -1,13 +1,19 @@
 class SyncChannel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
   def receive(data)
+    username = data['sent_by']
+    page = data['body']['page']
+    time = data['body']['time']
+    track = TimeTrack.joins(:user).find_by(users: {username: username}, page: page)
     
+    if track == nil
+      track = TimeTrack.new
+      track.time = time
+      user = User.find_by(username: username)
+      track.user = user
+      track.page = page
+    else
+      track.time += time
+    end
+    track.save
   end
 end
