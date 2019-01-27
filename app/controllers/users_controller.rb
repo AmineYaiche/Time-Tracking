8ec23fa6333d395e6eae97e61a64673e0df9cd59
Amine Yaiche <yaiche.amin@gmail.com>
2019-01-27 22:24:07 +0100

    Implementing signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
-  before_action :require_admin
+  before_action :require_admin, except: [:new, :create]
 
   def index
     @users = User.where(user_type: 'regular').paginate(page: params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.user_type = 'regular'
+    if @user.save
+      flash[:info] = "User account created successfully."
+      redirect_to root_url
+    else
+      flash[:danger] = @user.save!
+      render 'new'
+    end
   end
 
   def show
@@ -11,5 +27,10 @@ class UsersController < ApplicationController
     @user.time_track.each do |t|
       @data << [t.page, t.time]
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'root#index'
   get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
   get '/login', to: 'sessions#new'
+  get '/signup', to: 'users#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :users, only: [:index, :show]
